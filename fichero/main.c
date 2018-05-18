815a1506a6675faf1086fe188df5d0bd85285afe
destractor12 <36230522+destractor12@users.noreply.github.com>
2018-05-18 12:18:30 -0300

    Add files via upload

---
+++ fichero/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *fp;
+
+    char buffer[100]= "Esto es un texto dentro del fichero.";
+    char bufferIn[100];
+
+
+
+    fp= fopen("C:\\Users\\alumno\\Desktop\\Nueva carpeta\\fichero.txt","w");
+
+
+    fclose(fp);
+    system("pause");
+    system("cls");
+
+    fp= fopen("C:\\Users\\alumno\\Desktop\\Nueva carpeta\\fichero.txt","r");
+    if(fp==NULL)
+    {
+        printf("Archivo no encontrado!!!");
+    }
+    else
+    {
+        while(!feof(fp))
+        {
+            fgets(bufferIn,100,fp);
+            puts(bufferIn);
+        }
+    }
+
+
+
+
+
+    return 0;
+}
